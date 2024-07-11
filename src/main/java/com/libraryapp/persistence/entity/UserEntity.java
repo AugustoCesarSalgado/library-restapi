3c868cf336a5bf3ba3bebd27c37a79981c66687f
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-07-11 19:27:07 +0200

    UserEntity created

---
+++ src/main/java/com/libraryapp/persistence/entity/UserEntity.java
@@ -0,0 +1,34 @@
+package com.libraryapp.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+
+    @Column(name = "is_enabled")
+    private boolean isEnabled;
+
+    @Column(name = "account_non_expired")
+    private boolean accountNonExpired;
+
+    @Column(name = "account_non_locked")
+    private boolean accountNonLocked;
+
+    @Column(name = "credentials_non_expired")
+    private boolean credentialsNonExpired;
+
+}
