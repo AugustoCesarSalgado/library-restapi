deea0e7ff60e9bbad46deb15751c6557255ee902
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-07-11 19:37:28 +0200

    CategoryEntity created

---
+++ src/main/java/com/libraryapp/persistence/entity/CategoryEntity.java
@@ -0,0 +1,21 @@
+package com.libraryapp.persistence.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
