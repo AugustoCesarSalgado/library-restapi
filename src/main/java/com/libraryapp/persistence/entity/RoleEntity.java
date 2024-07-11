3c00435bca63a58c75c602f30617184d89ab369d
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-07-11 19:31:24 +0200

    RoleEntity and RoleEnum created

---
+++ src/main/java/com/libraryapp/persistence/entity/RoleEntity.java
@@ -0,0 +1,26 @@
+package com.libraryapp.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    @Enumerated(EnumType.STRING)
+    private RoleEnum roleEnum;
+
+}
---
+++ src/main/java/com/libraryapp/persistence/entity/RoleEnum.java
@@ -0,0 +1,9 @@
+package com.libraryapp.persistence.entity;
+
+public enum RoleEnum {
+
+    ADMIN,
+    USER,
+    INVITED
+
+}
