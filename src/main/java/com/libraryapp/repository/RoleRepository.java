ad3de50d339b88d241f9504d2057b9d21ac90778
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-07-14 01:08:45 +0200

    RoleRepository created

---
+++ src/main/java/com/libraryapp/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.libraryapp.repository;
+
+import com.libraryapp.entity.RoleEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public interface RoleRepository extends CrudRepository<RoleEntity, Long> {
+
+    List<RoleEntity> findRoleEntitiesByRoleEnumIn(List<String> roleNames);
+
+}
