14cb339dacdc282404b3037545b54daec79bfeb3
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-07-15 01:13:22 +0200

    findById method created in CategoryController

--- src/main/java/com/libraryapp/controller/CategoryController.java
+++ src/main/java/com/libraryapp/controller/CategoryController.java
@@ -1,15 +1,18 @@
 package com.libraryapp.controller;
 
 import com.libraryapp.controller.dto.CategoryDTO;
+import com.libraryapp.entity.CategoryEntity;
 import com.libraryapp.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/library/category")
@@ -29,6 +32,25 @@ public class CategoryController {
                         .build())
                 .toList();
         return new ResponseEntity<>(categoryList, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        Optional<CategoryEntity> categoryOptional = categoryService.findById(id);
+
+        if (categoryOptional.isPresent()) {
+            CategoryEntity category = categoryOptional.get();
+
+            CategoryDTO categoryDTO = CategoryDTO.builder()
+                    .id(category.getId())
+                    .name(category.getName())
+                    .books(category.getBooks())
+                    .build();
+
+            return new ResponseEntity<>(categoryDTO, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
