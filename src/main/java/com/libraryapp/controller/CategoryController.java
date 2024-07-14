1a34a2b8e23b71e296774f1756b5b7cb8b61a244
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-07-15 01:30:20 +0200

    save method created in CategoryController

--- src/main/java/com/libraryapp/controller/CategoryController.java
+++ src/main/java/com/libraryapp/controller/CategoryController.java
@@ -6,11 +6,10 @@ import com.libraryapp.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
@@ -51,6 +50,19 @@ public class CategoryController {
         }
 
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody CategoryDTO categoryDTO) throws URISyntaxException {
+        if (categoryDTO.getName().isBlank()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        categoryService.save(CategoryEntity.builder()
+                .name(categoryDTO.getName())
+                .build());
+
+        return new ResponseEntity<>(new URI("/library/category/save"), HttpStatus.CREATED);
     }
 
 }
