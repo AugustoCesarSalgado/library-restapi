ac0217bdedf89af02aa86de119a15509908304d7
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-07-17 20:16:28 +0200

    save method added in BookController

--- src/main/java/com/libraryapp/controller/BookController.java
+++ src/main/java/com/libraryapp/controller/BookController.java
@@ -6,11 +6,10 @@ import com.libraryapp.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
@@ -54,6 +53,25 @@ public class BookController {
         }
 
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody BookDTO bookDTO) throws URISyntaxException {
+        if (bookDTO.getTitle().isBlank()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        BookEntity book = BookEntity.builder()
+                .title(bookDTO.getTitle())
+                .author(bookDTO.getAuthor())
+                .year(bookDTO.getYear())
+                .category(bookDTO.getCategory())
+                .build();
+
+        bookService.save(book);
+
+        return new ResponseEntity<>(new URI("/library/book/save"), HttpStatus.CREATED);
+
     }
 
 }
