720d5b29c5437955f0263bae561972e1aa4918eb
Augusto Salgado <augustocesarsalgado05@gmail.com>
2024-07-17 19:18:38 +0200

    Methods findAll and findById added in BookController

--- src/main/java/com/libraryapp/controller/BookController.java
+++ src/main/java/com/libraryapp/controller/BookController.java
@@ -1,9 +1,18 @@
 package com.libraryapp.controller;
 
+import com.libraryapp.controller.dto.BookDTO;
+import com.libraryapp.entity.BookEntity;
 import com.libraryapp.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/library/book")
@@ -11,5 +20,40 @@ public class BookController {
 
     @Autowired
     private IBookService bookService;
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll() {
+        List<BookDTO> bookList = bookService.findAll()
+                .stream()
+                .map(book -> BookDTO.builder()
+                        .id(book.getId())
+                        .title(book.getTitle())
+                        .author(book.getAuthor())
+                        .year(book.getYear())
+                        .category(book.getCategory())
+                        .build())
+                .toList();
+        return new ResponseEntity<>(bookList, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        Optional<BookEntity> bookOptional = bookService.findById(id);
+
+        if (bookOptional.isPresent()) {
+            BookEntity book = bookOptional.get();
+
+            BookDTO bookDTO = BookDTO.builder()
+                    .id(book.getId())
+                    .title(book.getTitle())
+                    .author(book.getAuthor())
+                    .year(book.getYear())
+                    .category(book.getCategory())
+                    .build();
+            return new ResponseEntity<>(bookDTO, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
 }
